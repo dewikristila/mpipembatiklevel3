d942f044cfaeaf1d02d92b7bc9e8e7e679de7bbc
dewikristila <dewikristila@gmail.com>
2024-09-28 22:04:50 +0800

    KIRIMMPI

---
+++ data/slide1.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{BCD9D831-0CB9-47D1-AD3C-46060D4E4692}'];loadHandler&&loadHandler(0, '<div id="spr0_392b4545"><div id="spr1_392b4545" class="kern slide"><img id="img0_392b4545" src="data/img0.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_392b4545" class="kern slide"></div></div>', '{"s":[]}');})();
